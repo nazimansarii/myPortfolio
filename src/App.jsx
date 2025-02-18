a3cb325442153d3d1333442c8bde65d7e27409f4
nazim ansari <codernajim@gmail.com>
2025-02-18 10:08:47 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import SplashCursor from "./components/reactbits/Animations/SplashCursor/SplashCursor";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <main className="bg-[#060606] max-w-[1500px] mx-auto">
+      <SplashCursor />
+
+      <div>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+        <h1>sfd</h1>
+      </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
