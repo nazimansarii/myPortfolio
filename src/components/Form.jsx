5d1053ffa89767666762ad429f5767671acfd436
nazim ansari <codernajim@gmail.com>
2025-02-26 18:38:19 +0530

    remove index fallback file from public and modify form to static

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -395,97 +395,111 @@ export default function Form() {
 
         <div className="flex  justify-center items-center  w-full md:w-1/2  text-white ">
           <form
-            autoComplete="off"
-            className="py-6 text-white  px-4  w-full flex justify-center items-center flex-col"
-            onSubmit={handleSubmit}
             name="contact"
             method="POST"
             data-netlify="true"
             netlify-honeypot="bot-field"
             action="/success"
+            class="py-6 text-white px-4 w-full max-w-md flex flex-col"
           >
             <input type="hidden" name="form-name" value="contact" />
 
-            {/* Honeypot field (invisible) */}
-            <p className="hidden">
+            <p class="hidden">
               <label>
                 Don’t fill this out if you're human: <input name="bot-field" />
               </label>
             </p>
-            <Input
-              label="Your Name"
-              icons={
+
+            <div class="mb-4">
+              <label class="block mb-2" for="name">
+                Your Name
+              </label>
+              <div class="flex items-center bg-[#333] p-2 rounded-md">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
                   height="20"
-                  viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="mr-2"
                 >
                   <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                   <circle cx="12" cy="7" r="4"></circle>
                 </svg>
-              }
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              error={errors.name}
-            />
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Your Name"
+                  class="w-full bg-transparent outline-none text-white"
+                  required
+                />
+              </div>
+            </div>
 
-            <Input
-              label="Your Email"
-              icons={
+            <div class="mb-4">
+              <label class="block mb-2" for="email">
+                Your Email
+              </label>
+              <div class="flex items-center bg-[#333] p-2 rounded-md">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
                   height="20"
-                  viewBox="0 0 24 24"
                   fill="none"
-                  stroke="#ffffff"
-                  strokeWidth="1"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="mr-2"
                 >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                   <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
                   <path d="M3 7l9 6l9 -6"></path>
                 </svg>
-              }
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              error={errors.email}
-            />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Your Email"
+                  class="w-full bg-transparent outline-none text-white "
+                  required
+                />
+              </div>
+            </div>
 
-            <Message
-              label="Your Message"
-              icons={
+            <div class="mb-4">
+              <label class="block mb-2" for="message">
+                Your Message
+              </label>
+              <div class="flex items-start bg-[#333] p-2 rounded-md pl-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
                   height="20"
-                  viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-message-square"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="mr-2  mt-1"
                 >
                   <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
                 </svg>
-              }
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              error={errors.message}
-            />
+                <textarea
+                  name="message"
+                  placeholder="Your Message"
+                  class="w-full bg-transparent outline-none text-white"
+                  rows="4"
+                  required
+                ></textarea>
+              </div>
+            </div>
 
-            <button className="bg-[#1788AE] w-full py-2 mt-4 rounded-md cursor-pointer transform hover:scale-101 transition-transform duration-500">
+            <button
+              type="submit"
+              class="bg-[#1788AE] w-full py-2 mt-4 rounded-md cursor-pointer transform hover:scale-101 transition-transform duration-500"
+            >
               Send Message
             </button>
           </form>
